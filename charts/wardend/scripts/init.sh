c5b0ff4e3dd583c0c496f5bedf84e21eb7fbb28e
Joonas Lehtimaki <joonas.lehtimaki@gmail.com>
2024-04-16 18:03:11 +0300

    feat: wardend to allow new networks

--- charts/wardend/scripts/init.sh
+++ charts/wardend/scripts/init.sh
@@ -12,16 +12,14 @@ type jq || exit 1
 #Check if Home data exists, if not create it.
 if [ ! -d "$WARDEND_HOME/data" ]; then
   if [[ "$SYNC_METHOD" == "scratch" ]]; then
-    /usr/bin/wardend init --chain-id "$WARDEND_CHAIN_ID" --moniker "$WARDEND_MONIKER"
-    mkdir -p "$WARDEND_HOME/config" || exit 1
-    curl -s "$WARDEND_NET/genesis.json" > "$WARDEND_HOME/config/genesis.json"
+    /usr/bin/wardend init --chain-id "$WARDEND_CHAIN_ID" "$WARDEND_MONIKER"
+    if [[ ! -z $WARDEN_GENESIS_RPC ]]; then
+      curl -s "$WARDEN_GENESIS_RPC/genesis.json" > "$WARDEND_HOME/config/genesis.json"
+    fi
   elif [[ "$SYNC_METHOD" == "snapshot" ]]; then
         mkdir $WARDEND_HOME
         curl -L $SNAPSHOT_URL | tar -xzvf - -C $WARDEND_HOME
   fi
 else
   echo "Found Warden data folder!"
-fi
-if [ -f "/scripts/node_key.json" ]; then
-  cp -f /scripts/node_key.json $WARDEND_HOME/config/node_key.json;
 fi
