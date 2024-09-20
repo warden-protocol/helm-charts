c70979c11061ef0f42794f2a1375373b6c437f15
Joonas Lehtimaki <joonas.lehtimaki@gmail.com>
2024-09-20 16:23:37 +0300

    feat(horcrux): add delete option to the init script

--- charts/horcrux/scripts/init.sh
+++ charts/horcrux/scripts/init.sh
@@ -6,6 +6,24 @@ SECRETS=/tmp/secrets
 mkdir -p $HORCRUX_HOME/state
 chown -fR 2345:2345 $HORCRUX_HOME
 
+# Check pruned chains
+for file in "$HORCRUX_HOME"/state/*; do
+    if [ -f "$file" ]; then
+        # Check if the file contains the word "DELETE"
+        if grep -q "DELETE" "$file"; then
+            echo "File $(basename "$file") contains 'DELETE'. Deleting..."
+            mv "$file" "$file".deleted
+            if [ $? -eq 0 ]; then
+                echo "File $(basename "$file") deleted successfully."
+            else
+                echo "Failed to delete $(basename "$file")."
+            fi
+        else
+            echo "File $(basename "$file") does not contain 'DELETE'."
+        fi
+    fi
+done
+
 # Check that all files are present
 for file in "$SECRETS"/*; do
     # Get the base name of the file
